e1537d012f9625fc3e5c2f72827cebb9fed42164
ajaynegi45 <ajaynegi3345@icloud.com>
2025-01-27 03:06:39 +0530

    Add robot.txt and Sitemap and update SEO also

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ajaynegi.web.app',
+  site: 'https://ajaynegi.web.app',
+  integrations: [sitemap()],
 });
