b6e4e91e3d702cfd2151869930fc7ecc7d9ebc6e
ajaynegi45 <ajaynegi3345@icloud.com>
2025-01-27 14:50:34 +0530

    Improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ajaynegi.web.app',
-  integrations: [sitemap()],
+    site: 'https://ajaynegi.web.app',
+    integrations: [sitemap()],
+    markdown: {
+        shikiConfig: {
+            theme: 'vitesse-dark',
+        },
+    },
 });
+
+
+// themes: {
+//   light: 'github-light',
+//   dark: 'github-dark',
+// },
