daa336c0e8f452a272b549e4582cf6f30c9a123e
ajaynegi45 <ajaynegi3345@icloud.com>
2025-01-28 22:11:54 +0530

    add sound effect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://ajaynegi.web.app',
-    integrations: [sitemap()],
+    integrations: [mdx(), sitemap()],
     markdown: {
+        syntaxHighlight: 'prism',
         shikiConfig: {
             theme: 'vitesse-dark',
         },
