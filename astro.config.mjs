1e2900636aef06ec62c2c77ae7a6f286d0a9454e
ajaynegi45 <ajaynegi3345@icloud.com>
2025-06-06 01:25:42 +0530

    new spotlight feature added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import sentry from '@sentry/astro';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,8 +10,20 @@ export default defineConfig({
     integrations: [
         mdx(),
         sitemap({
-                xslURL: '/sitemap.xsl'
-        })
+            xslURL: '/sitemap.xsl'
+    }),
+        sentry({
+            dsn: "https://edaf914c0e159f4063a4c0b410538bff@o4509439190171648.ingest.de.sentry.io/4509439193448528",
+            // Setting this option to true will send default PII data to Sentry.
+            // For example, automatic IP address collection on events
+            // @ts-ignore
+            sendDefaultPii: true,
+            sourceMapsUploadOptions: {
+                project: "javascript-astro",
+                org: "codies-coder",
+                authToken: process.env.SENTRY_AUTH_TOKEN,
+            },
+        }),
     ],
     markdown: {
         shikiConfig: {
@@ -18,4 +31,3 @@ export default defineConfig({
         },
     },
 });
-
